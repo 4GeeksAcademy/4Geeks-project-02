dac1cca3751e1037e518d7012abed535bb97bacb
Vanesa Juarez Paris <131259155+vanesascode@users.noreply.github.com>
2023-06-19 11:50:19 +0000

    Final upload

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ window.onload = function() {
     culpable = "A goat";
     document.body.style.backgroundColor = "purple";
   } else if (randomNumberculpable >= 4 / 5 && randomNumberculpable < 1) {
-    culpable = "An old dog";
+    culpable = "A dog";
     document.body.style.backgroundColor = "darkorange";
   }
 
